c480e4b5694d59160903a555e6a0d5d75a54ad4a
KimTaeHyeong17 <37135317+KimTaeHyeong17@users.noreply.github.com>
2018-05-15 18:00:54 +0900

    3rd commit

---
+++ Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/Fragment_Plan.java
@@ -0,0 +1,22 @@
+package com.example.uuzaz.daily_note_1;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class Fragment_Plan extends android.support.v4.app.Fragment {
+
+    public Fragment_Plan()
+    {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_plan, container, false);
+    }
+}
--- Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/MainActivity.java
+++ Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.uuzaz.daily_note_1;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Initializing the TabLayout
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("계획"));
+        tabLayout.addTab(tabLayout.newTab().setText("실행"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Initializing ViewPager
+        viewPager=(ViewPager) findViewById(R.id.pager);
+
+        //creating TabPageradapter adapter
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        // Set TabSelectedListener
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
+
 }
