27c8f23786b2f79c44f5fd0463e3df716bbb7554
KimTaeHyeong17 <37135317+KimTaeHyeong17@users.noreply.github.com>
2018-05-22 16:51:38 +0900

    checklist

--- Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/Fragment_Plan.java
+++ Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/Fragment_Plan.java
@@ -1,22 +1,54 @@
 package com.example.uuzaz.daily_note_1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Fragment_Plan extends android.support.v4.app.Fragment {
 
-    public Fragment_Plan()
-    {
+    ArrayAdapter<String> mTaskAdapter;
+
+    public Fragment_Plan() {
 
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_plan, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_plan, container, false);
+
+        //Create the fake data
+        String[] fakeData = {
+                "08:00~09:00", "09:00~10:00", "10:00~11:00", "11:00~12:00", "12:00~13:00", "13:00~14:00", "14:00~15:00",
+                "15:00~16:00", "16:00~17:00", "17:00~18:00", "18:00~19:00", "19:00~20:00", "20:00~21:00", "21:00~22:00",
+                "22:00~23:00", "23:00~24:00", "24:00~01:00", "01:00~02:00", "02:00~03:00", "03:00~04:00", "04:00~05:00",
+                "05:00~06:00", "06:00~07:00", "07:00~08:00"
+        };
+        List<String> tasks = new ArrayList<String>(Arrays.asList(fakeData));
+
+        //Create the ArrayAdapter by specifying context, "how" (layout),"where" (textview), and " what" (data)
+        mTaskAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_task,
+                R.id.list_item_task_textview,
+                tasks);
+
+        //Still need to bind adapter to the ListView
+        ListView listView = (ListView) rootview.findViewById(R.id.listview_tasks);
+        listView.setAdapter(mTaskAdapter);
+
+        return rootview;
     }
 }
+
--- Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/MainActivity.java
+++ Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.uuzaz.daily_note_1;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
@@ -24,10 +33,10 @@ public class MainActivity extends AppCompatActivity
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         //Initializing ViewPager
-        viewPager=(ViewPager) findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.pager);
 
         //creating TabPageradapter adapter
-        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
@@ -49,6 +58,39 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        //set actionbar
+        //change actionbar title
+        getSupportActionBar().setTitle("DailyNote");
+        //homebutton
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
+    //앱타이들
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        ActionBar actionBar = getSupportActionBar();
+
+
+
+
+
+        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+
+
+        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
+
+        actionBar.setCustomView(actionbar);
+
+
+        return true;
+
+    }
 }
