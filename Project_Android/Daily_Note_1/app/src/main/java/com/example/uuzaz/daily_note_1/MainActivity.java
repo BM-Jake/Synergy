ebdbfb78207fd5e1e7894efaf9f02f4e6ce17c03
KimTaeHyeong17 <37135317+KimTaeHyeong17@users.noreply.github.com>
2018-05-25 01:44:25 +0900

    tired of this

--- Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/MainActivity.java
+++ Project_Android/Daily_Note_1/app/src/main/java/com/example/uuzaz/daily_note_1/MainActivity.java
@@ -1,36 +1,57 @@
 package com.example.uuzaz.daily_note_1;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Initializing the TabLayout
         tabLayout = (TabLayout) findViewById(R.id.tablayout);
         tabLayout.addTab(tabLayout.newTab().setText("계획"));
         tabLayout.addTab(tabLayout.newTab().setText("실행"));
+
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //upside down
+        TextView tv1 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0)).getChildAt(1));
+        tv1.setScaleY(-1);
+        TextView tv2 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(1)).getChildAt(1));
+        tv2.setScaleY(-1);
 
         //Initializing ViewPager
         viewPager = (ViewPager) findViewById(R.id.pager);
@@ -49,48 +70,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
         });
 
-        //set actionbar
-        //change actionbar title
-        getSupportActionBar().setTitle("DailyNote");
-        //homebutton
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-    }
-
-    //앱타이들
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        ActionBar actionBar = getSupportActionBar();
-
-
-
-
-
-        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
-        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
-        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
-
-
-        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
-
-        actionBar.setCustomView(actionbar);
-
-
-        return true;
 
     }
 }
+
+
+
+
